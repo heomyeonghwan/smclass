4e54bacd77bd94cfae88703636d85a785853b298
heomyeonghwan <gopsl3957@naver.com>
2024-11-22 13:20:33 +0900

    1122

---
+++ w1122/w01/board/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path,include
+from . import views
+
+
+app_name='board'
+urlpatterns = [
+    path('blist/', views.blist, name='blist'), # 게시판리스트
+    path('bwrite/', views.bwrite, name='bwrite'), # 글쓰기
+    path('bview/<int:bno>/', views.bview, name='bview'), # 글 상세보기
+    path('bmodify/<int:bno>/', views.bmodify, name='bmodify'), # 글 수정
+]
